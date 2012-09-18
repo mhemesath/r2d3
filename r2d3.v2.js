64179c98acec82b4d8738f7937dc915bf1747f82
Mike Hemesath <mike.hemesath@cerner.com>
2012-09-18 08:08:04 -0500

    lib/raphael/raphael.js

--- r2d3.v2.js
+++ r2d3.v2.js
@@ -2012,86 +2012,15 @@ d3_selectionPrototype.append = function(name) {
   name = d3.ns.qualify(name);
 
   function append() {
-    // Append called from raphael element
-    if (this.paper || this.raphael) {
-      return appendRaphaelElement(this.paper || this, this);
-
-    // Use Raphael to render SVG elements
-    } else if (name.local === 'svg') {
-      return appendRaphael(this);
-    }
-
+    if (name.local === 'svg') return appendRaphael(this);
 
     return this.appendChild(document.createElementNS(this.namespaceURI, name));
   }
 
   function appendNS() {
-    // Append called from raphael element
-    if (this.paper || this.raphael) {
-      return appendRaphaelElement(this.paper || this, this);
-
-    // Use Raphael to render SVG elements
-    } else if (name.local === 'svg') {
-      return appendRaphael(this);
-    }
+    if (name.local === 'svg') return appendRaphael(this);
 
     return this.appendChild(document.createElementNS(name.space, name.local));
-  }
-
-  function lineAttribute(name) {
-    return function(value) {
-      var attrs = this.data('lineAttrs');
-
-      // Isn't a line, return;
-      if (!attrs) return;
-
-      if (arguments.length < 1) {
-        return attrs[name];
-      }
-
-      attrs[name] = parseInt(value, 10);
-      if (!isNaN(attrs.x1) && !isNaN(attrs.y1) && !isNaN(attrs.x2) && !isNaN(attrs.y2)) {
-        this.attr('path', 'M' + attrs.x1 + ' ' + attrs.y1 + 'L' + attrs.x2 + ' ' + attrs.y2 + 'Z');
-      } else {
-        this.attr('path', null);
-      }
-    };
-  }
-
-  function appendRaphael(parent) {
-    var paper =  Raphael(parent, 0, 0);
-
-    paper.__attrs = { width: 0, height: 0 };
-
-    paper.ca.d = function(path) {
-      return { path: path };
-    };
-
-    paper.ca.x1 = lineAttribute('x1');
-    paper.ca.x2 = lineAttribute('x2');
-    paper.ca.y1 = lineAttribute('y1');
-    paper.ca.y2 = lineAttribute('y2');
-
-    // Fool sizzle into thinking the paper is an element
-    paper.nodeType = 1;
-    paper.nodeName = 'object';
-
-    return paper;
-  }
-
-
-  function appendRaphaelElement(paper, parent) {
-
-    var el = paper[name]();
-    // Ensure that sets have a paper reference
-    el.parentNode = el.paper = paper;
-
-    // If the parent is a Raphael Set, add the element to it
-    if (parent.type && parent.type === 'set') {
-      parent.push(el);
-    }
-
-    return el;
   }
 
   return this.select(name.local ? appendNS : append);
@@ -5075,125 +5004,216 @@ var d3_svg_brushResizes = [
   ["n", "s"],
   []
 ];
-(function() {
+function paperClassedAdd(node, name) {
+  var re = new RegExp("(^|\\s+)" + d3.requote(name) + "(\\s+|$)", "g");
 
-  if (typeof Raphael === 'undefined') return;
+  // Easy case, browser supports classList
+  if (node.classList) return node.classList.add(name);
 
-
-
-  function classedAdd(node, name) {
-          var re = new RegExp("(^|\\s+)" + d3.requote(name) + "(\\s+|$)", "g");
-          if (c = node.classList) return c.add(name);
-            var c = node.className || '',
-                cb = c.baseVal != null,
-                cv = cb ? c.baseVal : c;
-            re.lastIndex = 0;
-          if (!re.test(cv)) {
-            cv = d3_collapse(cv + " " + name);
-            if (cb) c.baseVal = cv;
-            else node.setAttribute('class', cv);
-          }
-        }
-
-
-  Raphael.fn.removeChild = function(el) {
-    el.remove();
-  };
-
-  Raphael.fn.line = function () {
-    var line =  this.path();
-    line.data('lineAttrs', { });
-    return line;
-  };
-
-
-  Raphael.fn.getAttribute = function(name) {
-    return this.__attrs[name];
-  };
-
-
-  Raphael.fn.setAttribute = function(name, value) {
-    this.__attrs[name] = value;
-
-    if (name === 'height' || name === 'width') {
-      this.setSize(this.__attrs.width, this.__attrs.height);
+  var c = node.className || '',
+      cb = c.baseVal !== null,
+      cv = cb ? c.baseVal : c;
+  re.lastIndex = 0;
+  if (!re.test(cv)) {
+    cv = d3_collapse(cv + " " + name);
+    if (cb) {
+      c.baseVal = cv;
+    } else {
+      node.setAttribute('class', cv);
     }
-  };
+  }
+}
 
 
-  Raphael.st.getElementsByClassName  = Raphael.fn.getElementsByClassName = function(selector) {
-    var matches = [];
-    selector = '.' + selector;
+function lineAttribute(name) {
+  return function(value) {
+    var attrs = this.data('lineAttrs');
 
-    this.forEach(function(el) {
-      if (Sizzle.matchesSelector(el.node, selector)) matches.push(el);
-    });
-    return matches;
-  };
+    // Isn't a line, return;
+    if (!attrs) return;
 
-  Raphael.st.getElementsByTagName = Raphael.fn.getElementsByTagName = function(tag) {
-    var matches = [];
-    this.forEach(function(el) {
-      var type = el.data('lineAttrs') ? 'line' : el.type;
-      if (type === tag) matches.push(el);
-    });
-    return matches;
-  };
-
-
-  Raphael.el.addEventListener = function(type, listener) {
-    this[type](listener);
-  };
-
-  Raphael.st.addEventListener = function(type, listener) {
-    this.forEach(function(el) {
-      el.addEventListener(type, listener);
-    });
-  };
-
-  Raphael.el.removeEventListener = function(type, listener) {
-    this['un'+ type](listener);
-  };
-
-  Raphael.st.removeEventListener = function(type, listener) {
-    this.forEach(function(el) {
-      el.removeEventListener(type, listener);
-    });
-  };
-
-
-  Raphael.el.setAttribute = function(name, value) {
-
-    if (name == 'class') {
-      classedAdd(this.node, value);
+    if (arguments.length < 1) {
+      return attrs[name];
     }
 
-    this.attr(name, value);
-    return this;
+    attrs[name] = parseInt(value, 10);
+    if (!isNaN(attrs.x1) && !isNaN(attrs.y1) && !isNaN(attrs.x2) && !isNaN(attrs.y2)) {
+      this.attr('path', 'M' + attrs.x1 + ' ' + attrs.y1 + 'L' + attrs.x2 + ' ' + attrs.y2 + 'Z');
+    } else {
+      this.attr('path', null);
+    }
+  };
+}
+
+
+function appendRaphael(parent) {
+  var paper =  Raphael(parent, 0, 0);
+
+  paper.__attrs = { width: 0, height: 0 };
+
+  paper.ca.d = function(path) {
+    return { path: path };
   };
 
-  Raphael.st.setAttribute = function(name, value) {
-    this.forEach(function(el) {
-      el.setAttribute(name, value);
-    });
+  paper.ca.x1 = lineAttribute('x1');
+  paper.ca.x2 = lineAttribute('x2');
+  paper.ca.y1 = lineAttribute('y1');
+  paper.ca.y2 = lineAttribute('y2');
+
+  // Fool sizzle into thinking the paper is an element
+  paper.nodeType = 1;
+  paper.nodeName = 'object';
+
+  return paper;
+}
+
+
+//========================================
+// Paper Extensions
+
+Raphael.fn.removeChild = function(el) {
+  el.remove();
+};
+
+
+Raphael.fn.line = function () {
+  var line =  this.path();
+  line.data('lineAttrs', { });
+  return line;
+};
+
+
+Raphael.fn.getAttribute = function(name) {
+  return this.__attrs[name];
+};
+
+
+Raphael.fn.setAttribute = function(name, value) {
+  this.__attrs[name] = value;
+
+  if (name === 'height' || name === 'width') {
+    this.setSize(this.__attrs.width, this.__attrs.height);
+  }
+};
+
+
+Raphael.fn.getElementsByClassName = function(selector) {
+  var matches = [];
+  selector = '.' + selector;
+
+  this.forEach(function(el) {
+    if (Sizzle.matchesSelector(el.node, selector)) matches.push(el);
+  });
+  return matches;
+};
+
+
+Raphael.fn.getElementsByTagName = function(tag) {
+  var matches = [];
+  this.forEach(function(el) {
+    var type = el.data('lineAttrs') ? 'line' : el.type;
+    if (type === tag) matches.push(el);
+  });
+  return matches;
+};
+
+
+Raphael.fn.appendChild = function(childNode) {
+  var type = childNode && childNode.nodeName,
+      node =  type ? this[type.toLowerCase()]() : null;
+
+  // Ensure Paper can be referenced from sets
+  if (node) {
+    node.paper = this;
+  }
+  return node;
+};
+
+
+
+
+//========================================
+// Element Extensions
+
+Raphael.el.addEventListener = function(type, listener) {
+  this[type](listener);
+};
+
+
+Raphael.el.removeEventListener = function(type, listener) {
+  this['un'+ type](listener);
+};
+
+
+Raphael.el.setAttribute = function(name, value) {
+
+  if (name == 'class' || name == 'className') {
+    paperClassedAdd(this.node, value);
   }
 
-  Raphael.el.removeAttribute = function(name) {
-    this.attr(name, '');
-    return this;
-  };
+  this.attr(name, value);
+  return this;
+};
 
-  Raphael.st.removeAttribute = function(name) {
-    this.forEach(function(el) {
-      el.removeAttribute(name);
-    });
-  };
 
-  Raphael.el.getAttribute = function(name) {
-    return this.attr(name);
-  };
+Raphael.el.removeAttribute = function(name) {
+  this.attr(name, '');
+  return this;
+};
 
-}());
+
+Raphael.el.getAttribute = function(name) {
+  return this.attr(name);
+};
+
+
+
+
+
+
+//========================================
+// Set Extensions
+
+Raphael.st.getElementsByClassName  = Raphael.fn.getElementsByClassName;
+
+
+Raphael.st.getElementsByTagName = Raphael.fn.getElementsByTagName;
+
+
+Raphael.st.appendChild = function(childNode) {
+  var node = this.paper.appendChild(childNode);
+  this.push(node);
+  return node;
+};
+
+
+Raphael.st.addEventListener = function(type, listener) {
+  this.forEach(function(el) {
+    el.addEventListener(type, listener);
+  });
+};
+
+
+Raphael.st.removeEventListener = function(type, listener) {
+  this.forEach(function(el) {
+    el.removeEventListener(type, listener);
+  });
+};
+
+
+Raphael.st.setAttribute = function(name, value) {
+  this.forEach(function(el) {
+    el.setAttribute(name, value);
+  });
+};
+
+
+Raphael.st.removeAttribute = function(name) {
+  this.forEach(function(el) {
+    el.removeAttribute(name);
+  });
+};
+
 d3.behavior = {};
 // TODO Track touch points by identifier.
 
