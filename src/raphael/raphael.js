b462dd1ab9c8fe43ddcdab4efb5de4daad6e76f7
Robin Houston <robin.houston@gmail.com>
2012-11-08 10:44:31 +0000

    Whoops
    Only override getComputedStyle with a fake version if it does not
    already exist.

--- src/raphael/raphael.js
+++ src/raphael/raphael.js
@@ -3,20 +3,22 @@
 (function(defaultGetComputedStyle) {
   // If we don't have window.getComputedStyle, as in IE7,
   // make a pretend one.
-  defaultGetComputedStyle = function(el, pseudo) {
-    this.el = el;
-    this.getPropertyValue = function(prop) {
-      var re = /(\-([a-z]){1})/g;
-      if (prop == 'float') prop = 'styleFloat';
-      if (re.test(prop)) {
-        prop = prop.replace(re, function () {
-          return arguments[2].toUpperCase();
-        });
+  if (typeof defaultGetComputedStyle === "undefined") {
+    defaultGetComputedStyle = function(el, pseudo) {
+      this.el = el;
+      this.getPropertyValue = function(prop) {
+        var re = /(\-([a-z]){1})/g;
+        if (prop == 'float') prop = 'styleFloat';
+        if (re.test(prop)) {
+          prop = prop.replace(re, function () {
+            return arguments[2].toUpperCase();
+          });
+        }
+        return el.currentStyle[prop] ? el.currentStyle[prop] : null;
       }
-      return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-    }
-    return this;
-  };
+      return this;
+    };
+  }
   
   window.getComputedStyle = function(node) {
     // Override for Raphael
