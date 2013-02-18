abb596a5b0e9ef4db1a45a356c701d53014b9a68
Mike Hemesath <mike.hemesath@cerner.com>
2013-02-18 09:34:04 -0600

    Cached created nodes to boost IE performance

--- r2d3.v2.js
+++ r2d3.v2.js
@@ -7556,6 +7556,31 @@ if (!Array.prototype.filter)
     };
 }
 
+if (!Array.prototype.reduce) {
+  Array.prototype.reduce = function reduce(accumulator){
+    if (this===null || this===undefined) throw new TypeError("Object is null or undefined");
+    var i = 0, l = this.length >> 0, curr;
+ 
+    if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+      throw new TypeError("First argument is not callable");
+ 
+    if(arguments.length < 2) {
+      if (l === 0) throw new TypeError("Array length is 0 and no second argument");
+      curr = this[0];
+      i = 1; // start accumulating at the second element
+    }
+    else
+      curr = arguments[1];
+ 
+    while (i < l) {
+      if(i in this) curr = accumulator.call(undefined, curr, this[i], i, this);
+      ++i;
+    }
+ 
+    return curr;
+  };
+}
+
 if (!String.prototype.trim) {
     String.prototype.trim = function() {
       return this.replace(/^\s+|\s+$/g, '');
@@ -9676,19 +9701,46 @@ d3_selectionPrototype.html = function(value) {
 };
 // TODO append(node)?
 // TODO append(function)?
+
+/**
+ * Gives IE a performance boost on creating elements from clone
+ * rather than from scratch.
+ */
+var createElementFromCache = (function() {
+  var cache = {},
+      fragmentDiv = document.createElement('div');
+      fragmentDiv.style.display = "none";
+  
+  return function(ns, name) {   
+    if (fragmentDiv.parentNode !== document.body) {
+      document.body.appendChild(fragmentDiv); 
+    }
+     
+    if (cache[name] === undefined) {
+      cache[name] = document.createElementNS(ns, name);
+    }
+    
+    var clone;
+    fragmentDiv.innerHTML =  cache[name].outerHTML;
+    fragmentDiv.removeChild( clone = fragmentDiv.firstChild );
+    return clone;
+  };
+}());
+
+
 d3_selectionPrototype.append = function(name) {
   name = d3.ns.qualify(name);
 
   function append() {
     if (name.local === 'svg') return appendRaphael(this);
 
-    return this.appendChild(document.createElementNS(this.namespaceURI, name));
+    return this.appendChild(createElementFromCache(this.namespaceURI, name));
   }
 
   function appendNS() {
     if (name.local === 'svg') return appendRaphael(this);
 
-    return this.appendChild(document.createElementNS(name.space, name.local));
+    return this.appendChild(createElementFromCache(name.space, name.local));
   }
 
   return this.select(name.local ? appendNS : append);
--- src/core/selection-append.js
+++ src/core/selection-append.js
@@ -1,18 +1,45 @@
 // TODO append(node)?
 // TODO append(function)?
+
+/**
+ * Gives IE a performance boost on creating elements from clone
+ * rather than from scratch.
+ */
+var createElementFromCache = (function() {
+  var cache = {},
+      fragmentDiv = document.createElement('div');
+      fragmentDiv.style.display = "none";
+  
+  return function(ns, name) {   
+    if (fragmentDiv.parentNode !== document.body) {
+      document.body.appendChild(fragmentDiv); 
+    }
+     
+    if (cache[name] === undefined) {
+      cache[name] = document.createElementNS(ns, name);
+    }
+    
+    var clone;
+    fragmentDiv.innerHTML =  cache[name].outerHTML;
+    fragmentDiv.removeChild( clone = fragmentDiv.firstChild );
+    return clone;
+  };
+}());
+
+
 d3_selectionPrototype.append = function(name) {
   name = d3.ns.qualify(name);
 
   function append() {
     if (name.local === 'svg') return appendRaphael(this);
 
-    return this.appendChild(document.createElementNS(this.namespaceURI, name));
+    return this.appendChild(createElementFromCache(this.namespaceURI, name));
   }
 
   function appendNS() {
     if (name.local === 'svg') return appendRaphael(this);
 
-    return this.appendChild(document.createElementNS(name.space, name.local));
+    return this.appendChild(createElementFromCache(name.space, name.local));
   }
 
   return this.select(name.local ? appendNS : append);
