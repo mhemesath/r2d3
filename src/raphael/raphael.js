a913f86ca0ea32601a9affb0984eba8560533874
James Sun <james.sun@cerner.com>
2014-02-13 08:33:16 -0600

    Fixes for #103, #105, #106 - style, transition, text calls on HTML elements

--- src/raphael/raphael.js
+++ src/raphael/raphael.js
@@ -8,28 +8,28 @@
       } else {
         var camelCasedCssProperty = getCamelCasedCssProperty(cssProperty);
         if(el.currentStyle){
-          return el.currentStyle(camelCasedCssProperty);
+          return el.currentStyle[camelCasedCssProperty];
         } else {
           return el.style[camelCasedCssProperty];
         }
       }
     } else{
       return window.getComputedStyle(el).getPropertyValue(cssProperty);
-    }      
+    }
   }
-  
+
   function getCamelCasedCssProperty(cssProperty){
-    return cssProperty.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase() });
+    return cssProperty.replace(/-([a-z])/g, function (g) { return g.charAt(1).toUpperCase() });
   }
-  
+
 })(this);
 
 
 
 // Register SVG elements with IE so they can be styled
 (function() {
-  var svgElements = 'circle ellipse line polygon polyline rect g svg image path text'.split(' '); 
-    
+  var svgElements = 'circle ellipse line polygon polyline rect g svg image path text'.split(' ');
+
   for (var i=0; i< svgElements.length; i++) {
     document.createElement(svgElements[i]);
   }
@@ -39,12 +39,12 @@
 function appendRaphael(parent) {
   var paper =  Raphael(parent, 0, 0),
       svg = document.createElement('svg');
-      
+
   // Create the DOM node representing the SVG docuemnt
   // This will enable us to pull in styles from the stylesheets using
   // node.currentStyle
   svg.style.display = 'none';
   parent.appendChild(svg);
-  
+
   return new R2D3Element(paper, svg);
 }
--- tests/core/text-tests.js
+++ tests/core/text-tests.js
@@ -4,19 +4,25 @@ module( "Text", {
     }
 });
 
-
 test( "text", function() {
   var svg = d3.select('#text').append('svg'),
       el = svg.append('text');
-      
-  el.text('hello')
-  equal(el.text(), 'hello')
+
+  el.text('hello');
+  equal(el.text(), 'hello');
 });
 
 test("text function", function() {
   var svg = d3.select('#text').append('svg'),
       el = svg.append('text');
-      
-  el.text(function() { return "hello"; })
-  equal(el.text(), 'hello')
+
+  el.text(function() { return "hello"; });
+  equal(el.text(), 'hello');
+});
+
+test("text on HTML element", function() {
+  var div = d3.select('#text').append('div');
+
+  div.text('hello');
+  equal(div.text(), 'hello');
 });
